02849712257eb896316024afc0b450523a91b6cd
Gökhan esen <gokhan_esen@hotmail.com.tr>
2025-01-04 13:09:56 +0300

    database design and migration

--- ECommerceAPI/ECommerceAPI.API/Program.cs
+++ ECommerceAPI/ECommerceAPI.API/Program.cs
@@ -1,4 +1,4 @@
-
+using ECommerceAPI.Persistence;
 namespace ECommerceAPI.API
 {
     public class Program
@@ -7,15 +7,16 @@ namespace ECommerceAPI.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+
+
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+
+            builder.Services.AddPersistenceServices(builder.Configuration);
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
---
+++ ECommerceAPI/ECommerceAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using ECommerceAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ECommerceAPIDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("PostgreSQL")));
+
+            return services;
+        }
+    }
+}
