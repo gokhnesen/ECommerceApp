78166b57a5a5e8ca4e8e4a8b0ac75114e18ce89f
Gökhan esen <gokhan_esen@hotmail.com.tr>
2025-03-01 21:12:43 +0300

    adding some custom notifications

--- ECommerceClient/src/app/app.component.ts
+++ ECommerceClient/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $:any
 
 @Component({
@@ -8,4 +10,15 @@ declare var $:any
 })
 export class AppComponent {
   title = 'ECommerceClient';
+  /**
+   *
+   */
+  constructor(private toastrService: CustomToastrService) { 
+    toastrService.message("Sa","Gokhan",{messageType:ToastrMessageType.Info, position: ToastrPosition.TopCenter});
+    toastrService.message("Sa","Gokhan",{messageType:ToastrMessageType.Info, position: ToastrPosition.TopCenter}); 
+    toastrService.message("Sa","Gokhan",{messageType:ToastrMessageType.Info, position: ToastrPosition.TopCenter}); 
+    toastrService.message("Sa","Gokhan",{messageType:ToastrMessageType.Info, position: ToastrPosition.TopCenter}); 
+
+
+  }
 }
