7160dc3892df2bc0767cb4cdccac83ec88e44f02
Gökhan esen <gokhan_esen@hotmail.com.tr>
2025-03-03 13:21:46 +0300

    adding spinner animation

--- ECommerceClient/src/app/admin/components/dashboard/dashboard.component.ts
+++ ECommerceClient/src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
 
-  constructor() {
+  constructor(private spinner: NgxSpinnerService) {
   }
   ngOnInit(): void {
+    this.spinner.show();
   }
 
 }
